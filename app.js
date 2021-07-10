311a9bdf3a9bead6fd5d0546894a5b3ece9e4a04
Valentin Suarez <suarezvalentin2004@gmail.com>
2021-07-10 20:58:57 -0300

    prueba nro2

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
 const app = express();
+const path = require('path');
 
-app.listen(3030, () => {
-    console.log("Servidor corriendo");
-})
+
+
+const publicPath = path.resolve(dirname, "./public");
+app.use(express.static(publicPath));
+
+
+const publicPath2 = path.resolve(dirname, "./views");
+app.use(express.static(publicPath2));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
 
-const publicPath = path.resolve(dirname, "./public");
-app.use(express.static(publicPath));
 
-const publicPath2 = path.resolve(dirname, "./views");
-app.use(express.static(publicPath2));
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Corriendo servidor en el puerto 3000");
+})
